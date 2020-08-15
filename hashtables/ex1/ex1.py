7d1bb8a7f10b8cad0c24fe9b024072d3e0689067
robertelias <robert.elias@live.com>
2020-08-14 17:18:23 -0700

    Exercise 1 mvp met

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,13 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    items = {}
 
+    for i, weight in enumerate(weights):
+        weight_index = limit - weights[i]
+
+        if weight_index not in items:
+            items[weight] = i
+        else:
+            return (i, items[weight_index])
     return None
